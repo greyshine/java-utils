a37ca488895944e07be8d0a526a1d4bcba684b17
greyshine <kuemmel.dss@gmx.de>
2017-09-22 08:15:36 +0200

    test enhancements

--- src/test/java/de/greyshine/utils/beta/TikaUtilsTests.java
+++ src/test/java/de/greyshine/utils/beta/TikaUtilsTests.java
@@ -2,7 +2,10 @@ package de.greyshine.utils.beta;
 
 import java.io.File;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import de.greyshine.utils.deprecated.Utils;
 
 public class TikaUtilsTests {
 
@@ -10,17 +13,19 @@ public class TikaUtilsTests {
 	public void test() throws Exception {
 		
 		String theText = TikaUtils.pdfToString( new File("src/test/resources/tika/helloworld.pdf") );
-		//theText = TikaUtils.pdfToString( new File("src/test/resources/tika/copyprotected.pdf") );
+
+		Assert.assertEquals("Hello World!", theText.trim());
+		
+		StringBuilder sb = new StringBuilder();
 		
 		TikaUtils.parsePdf(new File("src/test/resources/tika/copyprotected.pdf"), (idx,line)->{
-			
-			System.out.println( idx+": "+ line );
-			
+			sb.append( idx+": "+ line );
 		});
 		
+		final String theMd5 = Utils.getMd5( sb.toString() );
+		sb.setLength(0);
+		Assert.assertEquals("4fb4078c62b2a0172fb2392e42eb82d5", theMd5);
 		
-		System.out.println("XXX: "+  theText );
-
 	}
 	
 	
